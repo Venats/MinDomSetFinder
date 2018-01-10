2cabfdc62054ce8c09c8c5424ac4d5cb6a81f304
Taylor Ferguson <tayfer95@gmail.com>
2018-01-10 13:16:44 -0800

    cleaned up some global variables and #defines that don't need to be there

--- FindMinDominatingSet.c
+++ FindMinDominatingSet.c
@@ -7,18 +7,6 @@
 #include "Util.h"
 #include "Graph.h"
 
-#define NMAX 2187
-#define DEG_MAX 16
-
-#define BLUE (-1)
-#define WHITE 0
-#define RED 1
-
-Node* verticesByNumChoice[DEG_MAX + 2];
-int numberOfVertices;
-int degree[DEG_MAX];
-int numChoice[DEG_MAX + 2];
-
 int GetOutputType(int argc, char const *argv[])
 {
   int outputType = -1;
@@ -47,7 +35,8 @@ int main(int argc, char const *argv[])
   while(graph != NULL)
   {
 
-    graph =  ReadGraph();
+    FreeGraph(graph);
+    graph = ReadGraph();
   }
   return 0;
 }
